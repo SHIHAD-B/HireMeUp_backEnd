376762242c24212d9c502b5ec624afdbfc1ecf80
SHIHAD-B <cyberguardians404@gmail.com>
2024-06-27 15:21:51 +0530

    feat:chat and company workflow added

---
+++ run-all.ps1
@@ -0,0 +1,8 @@
+npm concurrently `
+    "npm --prefix auth-service run start:dev" `
+    "npm --prefix chat-service run start:dev" `
+    "npm --prefix company-service run start:dev" `
+    "npm --prefix job-service run start:dev" `
+    "npm --prefix notification-service run start:dev" `
+    "npm --prefix subscription-service run start:dev" `
+    "npm --prefix user-service run start:dev"`
